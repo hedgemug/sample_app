c360298fc029fe5e5d7122d43706270a95c92b91
Dani Zaghian <dani.zaghian@gmail.com>
2018-01-14 10:22:55 -0800

    added multiple routes and more complex data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
 
   def hello_method
-    render json: {message: "hello"}
+    render json: {message: "hello", time: Time.now.strftime("%b %e, %l:%M %p")}
+  end
+
+  def goodbye_method
+    render json: {message: "goodbye", lucky_numbers: [8, 21, 35]}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get "/hello_url" => "pages#hello_method"
+  get "/goodbye_url" => "pages#goodbye_method"
 end
